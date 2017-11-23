6b6e69d1f82f54763b732186fad29b71b537a957
reetou <fillmasterson@gmail.com>
2017-11-24 01:13:55 +0300

    changed name for collection

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-export default mongoose.model('User', new Schema({
+export default mongoose.model('users', new Schema({
   name: { type: String, unique: true, required: true },
   telegramId: { type: String, unique: true, required: true },
   stats: { type: Object, required: true },
